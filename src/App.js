352b3b7c78a6e46c529725b9ed7e1ebcfd24b67e
christopher frantz <christopher.frantz@libertymutual.com>
2017-11-01 12:04:23 -0400

    exploring other ways to create a react component

--- src/App.js
+++ src/App.js
@@ -3,20 +3,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+// original component from create-react-app
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to InsProf Torch React</h1>
         </header>
         <p className="App-intro">
-         learn-react
+        insprof-torch-react: a simple reference app using react and redux.
         </p>
       </div>
     );
   }
 }
 
-export default App;
+
+// class component
+// class App extends Component {
+
+//     render() {
+//         return (
+//           <h1>Hello World Chris! from Class Component</h1>
+//         );
+//     }
+// }
+
+
+// stateless function component
+// const App = () => <h1>Hello World Chris! from Stateless Function Component</h1>
+
+export default App
